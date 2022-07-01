0839c9d9469bff4087d28aa2b1b29b82c5931f16
Yegor Shevchenko <egorhs19@gmail.com>
2022-07-01 15:11:28 +0300

    Change bucket structure

--- custom_hash_set.js
+++ custom_hash_set.js
@@ -6,23 +6,24 @@ const NUMBER_OF_ITERATIONS = 10 ** 5
 const MAX_ELEM = 10 ** 5
 
 let st = new Hashtable();
+
 for (let i = 0; i < NUMBER_OF_ITERATIONS; i++) {
     const cur_elem = getRandInt(0, MAX_ELEM);
     if (!st.containsKey(cur_elem)) {
         st.put(cur_elem, 1);
     }
 }
-
+// console.log(st.keys().length, right_st.size);
 // let new_st = new Hashtable();
 
-// for (let i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-//     new_st.put(i, 1);
-// }
+for (let i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+    new_st.put(i, 1);
+}
 
-// for (let i = 0; i < NUMBER_OF_ITERATIONS; i++) {
-//     if (!new_st.containsKey(i)) {
-//         console.log('Your set is not working');
-//     }
-// }
+for (let i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+    if (!new_st.containsKey(i)) {
+        console.log('Your set is not working');
+    }
+}
 
 
